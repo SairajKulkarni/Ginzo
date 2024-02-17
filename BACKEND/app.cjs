1b2cbd4b0ba6ac0f146adbaedbe5533987b076ad
SairajKulkarni <kulkarnisairaj.967@gmail.com>
2024-02-17 20:09:16 +0530

    stripe api key issue

--- BACKEND/app.cjs
+++ BACKEND/app.cjs
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const env = require("dotenv").config();
+const env = require("dotenv");
 
 env.config({ path: "BACKEND/config/config.env" });
 
