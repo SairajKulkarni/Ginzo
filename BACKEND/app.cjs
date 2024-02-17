2230b37df1272c874080a1cd81e7169eea6a838b
SairajKulkarni <kulkarnisairaj.967@gmail.com>
2024-02-17 20:02:03 +0530

    get api key problem solve

--- BACKEND/app.cjs
+++ BACKEND/app.cjs
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const env = require("dotenv");
+const env = require("dotenv").config();
 
 env.config({ path: "BACKEND/config/config.env" });
 
